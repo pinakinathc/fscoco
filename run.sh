3cefd129f34f930dc3c0ac41b136b4f5b471b842
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2022-07-11 15:02:13 +0100

    added remaining code for ECCV 2022

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cd /vol/research/sketchcaption/phd2/dataset_paper/scene-sketch-text
 
 source /vol/research/sketchcaption/miniconda/bin/activate pytorch
 
-python -m cnn_baseline.main
+python -m src.tbir_baseline.main --use_coco
